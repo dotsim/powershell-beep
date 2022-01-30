fb7068baa75cfd8e4cf20c927e5f4300839b993d
dotSim <98029604+dotsim@users.noreply.github.com>
2022-01-30 23:51:38 +0300

    Add files via upload

---
+++ bruh.ps1
@@ -0,0 +1,16 @@
+Function bruh {
+    [System.Console]::WriteLine("Coded by Devori");
+    [System.Console]::Beep(500, 100);
+    [System.Console]::Beep(700, 100);
+    [System.Console]::Beep(900, 100);
+    [System.Console]::Beep(1000, 100);
+    [System.Console]::Beep(3000, 100);
+    [System.Console]::Beep(2000, 100);
+    [System.Console]::Beep(4000, 100);
+    [System.Console]::Beep(3000, 100);
+    [System.Console]::Beep(2000, 100);
+    [System.Console]::Beep(1000, 100);
+    [System.Console]::Beep(900, 100);
+    [System.Console]::Beep(959, 100);
+}
+bruh;
